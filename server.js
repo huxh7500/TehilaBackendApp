e06347ee278b02737ee73833386226250e5a3674
huxh7500 <86956846+huxh7500@users.noreply.github.com>
2021-10-09 13:38:21 +0300

    new update

--- server.js
+++ server.js
@@ -17,8 +17,9 @@ dotenv.config()
 connectDB()
 
 const app = express()
-app.use(cors());
-
+app.use(cors({
+  origin: '*'
+}));
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'))
 }
