7d6324311c81666ae907adb335b5c9cd3be3f521
huxh7500 <86956846+huxh7500@users.noreply.github.com>
2021-10-09 15:16:32 +0300

    new update

--- server.js
+++ server.js
@@ -16,20 +16,8 @@ import uploadRoutes from './routes/uploadRouts.js'
 dotenv.config()
 connectDB()
 
-const allowedOrigins = ['localhost:3000',
-                      'tehilafrontendapp.herokuapp.com'];
 app.use(cors({
-  origin: function(origin, callback){
-    // allow requests with no origin 
-    // (like mobile apps or curl requests)
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){
-      const msg = 'The CORS policy for this site does not ' +
-                'allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  }
+  origin: 'https://tehilafrontendapp.herokuapp.com'
 }));
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'))
