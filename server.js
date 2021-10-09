e907e1b7022e985bc0d8a960aa7c208a253a08b7
huxh7500 <86956846+huxh7500@users.noreply.github.com>
2021-10-09 14:31:39 +0300

    new update

--- server.js
+++ server.js
@@ -16,12 +16,20 @@ import uploadRoutes from './routes/uploadRouts.js'
 dotenv.config()
 connectDB()
 
-const app = express()
+var allowedOrigins = ['http://localhost:3000',
+                      'https://tehilafrontendapp.herokuapp.com'];
 app.use(cors({
-   origin: "*",
-   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-   preflightContinue: false,
-   optionsSuccessStatus: 204
+  origin: function(origin, callback){
+    // allow requests with no origin 
+    // (like mobile apps or curl requests)
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      const msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
 }));
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'))
