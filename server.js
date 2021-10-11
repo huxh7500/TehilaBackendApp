7054f1aabd58e4ec6d20c91bd3e3af8fa3266f69
huxh7500 <86956846+huxh7500@users.noreply.github.com>
2021-10-11 18:54:08 +0300

    new update

--- server.js
+++ server.js
@@ -22,9 +22,9 @@ app.use(function (req,res, next) {
   next()
 })
 
-app.use(cors());
-
-
+app.use(cors({
+  origin: '*'
+}));
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'))
 }
