f229cc619c29aacfc61615929dc5cf87d1def17e
huxh7500 <86956846+huxh7500@users.noreply.github.com>
2021-10-12 21:15:34 +0300

    new update

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose"
 
 const connectDB = async()=>{
     try{
-        const conn = await mongoose.connect(process.env.MONGO_URL)
+        const conn = await mongoose.connect('mongodb+srv://huxh7500:Sabika12@cluster0.ygfxh.mongodb.net/proshop?retryWrites=true&w=majority')
         console.log(`MongoDB Connected: ${conn.connection.host}`)
     }catch(error){
         console.log(`Error: ${error.message}`)
